2f739a424a91d57ffb9ef1aef96e3f43b7cbcbe6
Дарья Мурова <murovadarya@gmail.com>
2022-06-14 11:57:31 +0300

    New commit

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from .routers import projects, users, auth, access, transactions, costs, account
 from fastapi.middleware.cors import CORSMiddleware
 
 
-models.Base.metadata.create_all(bind=engine)
+# models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
