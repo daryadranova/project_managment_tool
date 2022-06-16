b7b9dc26da6ce4cf5001239640344735ad429b85
Дарья Мурова <murovadarya@gmail.com>
2022-06-16 22:17:31 +0300

    New commit

--- app/main.py
+++ app/main.py
@@ -1,16 +1,13 @@
 from fastapi import FastAPI
-# from .database import engine
+# from . import models
+from .database import engine
 from .routers import projects, users, auth, access, transactions, costs, accounts
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi import Request
-from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
+
 
 # models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-templates = Jinja2Templates(directory="templates")
 
 origins = [
     "*"
@@ -30,10 +27,8 @@ app.include_router(auth.router)
 app.include_router(access.router)
 app.include_router(transactions.router)
 app.include_router(costs.router)
-app.include_router(accounts.router)
 
 
 @app.get("/")
-async def home(request: Request):
-    return templates.TemplateResponse(
-        "index.html", {"request": request})
+def root():
+    return 1
